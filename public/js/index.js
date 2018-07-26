e6c54dbed89034eed56bfb93b0337b9deda0a440
mahecode <maharanamahesh6@gmail.com>
2018-07-26 22:34:53 +0530

    Init commit

---
+++ public/js/index.js
@@ -0,0 +1,12 @@
+var socket = io();
+socket.on('connect', ()=>{
+  console.log('Connected to server');
+});
+
+socket.on('newMessage', (message)=>{
+  console.log('New message: ', message);
+})
+
+socket.on('disconnect', ()=>{
+  console.log('Disconnected from server');
+});
