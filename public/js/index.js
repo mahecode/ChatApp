e9d4aa75f357f37026b41b0caea45d8ae486c974
mahecode <maharanamahesh6@gmail.com>
2018-08-01 15:50:12 +0530

    added ui

--- public/js/index.js
+++ public/js/index.js
@@ -25,7 +25,7 @@ $('.message-form').on('submit', (e) =>{
     from: 'User',
     message: $('[name=message]').val()
   },()=>{
-
+    $('[name=message]').val("");
   });
 });
 
@@ -34,12 +34,15 @@ locationButton.on('click', function () {
   if(!navigator.geolocation){
     return alert('Geolocation not supported by your browser');
   }
+  locationButton.attr('disabled','disabled').text('Sending location...');
   navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     })
   }, function() {
+    locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fetch location');
   });
 });
